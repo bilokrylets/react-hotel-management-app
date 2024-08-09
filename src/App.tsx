3ce2a21ba9553e2c7e3d5c055c72fd96c10e6939
Yaroslav Bilokrylets <4believy@gmail.com>
2024-08-09 11:51:38 +0300

    feat: add styled-components and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/globalStyles';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: aliceblue;
+`;
+
+const Button = styled.button`
+  font-size: 1.5rem;
+  padding: 1.2rem 1.5rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-lg);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-500);
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
     <>
-      <p>Click on the Vite and React logos to learn more</p>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>the wild oasis</H1>
+        <Button onClick={() => alert('111')}>check</Button>
+
+        <Input type="number" placeholder="number of guests"></Input>
+      </StyledApp>
     </>
   );
 }
