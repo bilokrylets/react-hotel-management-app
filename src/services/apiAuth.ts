f8ebac0330cc387c75914012625edfd8a067f50f
Yaroslav Bilokrylets <yaroslav.bilokrylets@gmail.com>
2024-08-26 14:03:21 +0300

    feat: add update name,password,avatar for user

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,5 +1,5 @@
 import { Login } from '../types/login';
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 type SignUp = {
   fullName: string;
@@ -49,4 +49,36 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName)
+    updateData = {
+      data: { fullName },
+    };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: avatarError } = await supabase.storage
+    .from('avatars')
+    .upload(fileName, avatar);
+
+  if (avatarError) throw new Error(avatarError.message);
+
+  const { data: updatedUser, error: updateError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+  if (error) throw new Error(updateError.message);
+
+  return updatedUser;
 }
