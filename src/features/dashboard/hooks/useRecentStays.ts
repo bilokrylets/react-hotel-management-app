d9f9e42aacd5d8ac745d21250f2bf4f3781b0af8
Yaroslav Bilokrylets <yaroslav.bilokrylets@gmail.com>
2024-08-28 15:02:24 +0300

    feat: add today activity stats to dashboard

--- src/features/dashboard/hooks/useRecentStays.ts
+++ src/features/dashboard/hooks/useRecentStays.ts
@@ -17,7 +17,7 @@ export function useRecentStays() {
   });
 
   const confirmedStays = stays?.filter(
-    (stay) => stay.status === 'checked-in' || 'checked-out',
+    (stay) => stay.status === 'checked-in' || stay.status === 'checked-out',
   );
 
   return { isLoading, stays, confirmedStays, numDays };
