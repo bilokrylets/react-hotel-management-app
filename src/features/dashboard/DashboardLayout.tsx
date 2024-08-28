49c3f55d48167c332012b21ec3bf887baf129e73
Yaroslav Bilokrylets <yaroslav.bilokrylets@gmail.com>
2024-08-28 12:46:39 +0300

    feat: add filter for dashboard

--- src/features/dashboard/DashboardLayout.tsx
+++ src/features/dashboard/DashboardLayout.tsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { useRecentStays } from './hooks/useRecentStays';
+import Spinner from '../../ui/Spinner';
+import { useRecentBookings } from './hooks/useRecentBookings';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,20 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoadingStay } = useRecentStays();
+
+  if (isLoading && isLoadingStay) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's Activity </div>
+      <div>chart stay </div>
+      <div></div>
+    </StyledDashboardLayout>
+  );
+}
+export default DashboardLayout;
